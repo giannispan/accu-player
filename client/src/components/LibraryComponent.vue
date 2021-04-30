898366871104972be48149c608b890d22406609f
giannispan <giannis.panagiotopoulos@gmail.com>
2021-04-30 13:15:34 +0300

    upload form, showing errors

--- client/src/components/LibraryComponent.vue
+++ client/src/components/LibraryComponent.vue
@@ -1,6 +1,43 @@
 <template>
   <div>
-    
+    <form @submit.prevent="sendFile" enctype="multipart/form-data">
+      <div class="field">
+        <div class="file is-boxed is-primary">
+          <label class="label">
+            <input 
+              type="file" 
+              ref="file"
+              @change="selectFile"
+              accept="audio/*"
+              class="file-input"
+            />
+            <span class="file-cta">
+              <span class="file-icon">
+                <i class="fas fa-upload">
+
+                </i>
+              </span>
+              <span class="file-label">
+                Choose a file...
+              </span>
+            </span>
+
+            <span v-if="file" class="file-name">{{file.name}}</span>
+          </label>
+
+        </div>
+
+      </div>
+
+      <div class="field">
+        <button class="button is-info">Send</button>
+      </div>
+      <p class="error" v-if="error">{{error}}</p>
+    </form>
+    <hr>
+    <div class="content">
+        <h1 class="title">My audio list</h1>
+    </div>
   </div>
 </template>
 
@@ -34,6 +71,9 @@ export default {
             this.error = '';
           }, 2000);
         })
+    },
+    selectFile() {
+      this.file = this.$refs.file.files[0];
     } 
   },
   
@@ -42,5 +82,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .content {
+    width: 650px;
+    margin: auto;
+  }
+  .field {
+    display: flex;
+    justify-content: center;
+  }
+  .file-cta {
+    width: 100%;
+  }
+  h1 {
+    text-align: center;
+  }
+  p.error {
+    border: 1px solid #ff5b5f;
+    background-color: #ffc5c1;
+    padding:  10px;
+    margin-bottom: 15px;
+  }
 </style>
